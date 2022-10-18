a0e0d722455a06ca9ed10c0a99b8609c868fc71c
joongwon <wnddnjs823@ajou.ac.kr>
2022-10-18 09:57:10 +0900

    Initial Commit

---
+++ SeSACWeek1617/SimpleTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek1617
+//
+//  Created by 이중원 on 2022/10/18.
+//
+
+import UIKit
+
+class SimpleTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
